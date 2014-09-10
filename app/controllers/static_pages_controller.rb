a53b9749adeba9caa545714e0d09d2371bae37e3
Chintav Shah <sword4life4@gmail.com>
2014-09-10 02:59:20 -0400

    page updates

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,19 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+
+  def about
+  end
+
+  def experienced
+  end
+
+  def new_user
+  end
+
+  def contact
+  end
+end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PennApp::Application.config.session_store :cookie_store, key: '_penn_app_session'
